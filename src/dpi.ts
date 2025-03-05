b2554d5709312de6afb17df61eef6ae285a007bb
Oli Evans <oli@protocol.ai>
2025-03-05 22:38:33 +0000

    feat: country pages and better data-exchnage info format
    License: MIT
    Signed-off-by: Oli Evans <oli@zilla.org.uk>

---
+++ src/dpi.ts
@@ -0,0 +1,45 @@
+import dataJSON from './data-exchange.json'
+
+export const DataExchanges = dataJSON.map(x => {
+  return {
+    'Country': x['Country'],
+    'Name': x['Data exchange system name'],
+    'Status': x['Status of implementation'],
+    'National or Regional': x['National/ Regional'],
+    "Sector-specific or Cross-sectoral": x["Sector-specific/ Cross-sectoral"],
+    "Semantic interoperability": x["Semantic interoperability"],
+    "Real-time": x["Data is shared in (near) real-time through the Data Exchange"],
+    "Scalable architecture": x["Technology architecture of the DES is scalable"],
+    "Governing Entity": x["Public-interest entity governing the development and operations of the Data Exchange"],
+    "Ownership": x["Ownership"],
+    "Onboarding docs": "Unknown",
+    "Allowed users": x["Types of entities that can participate  in the data exchange"],
+    "Auditable": x["The system provides mechanisms to audit data exchanges"],
+    "Procedural rules established": x["Procedural rules for the Data Exchange (access restrictions, protections, etc.) are established"],
+    "Coordination unit": x["Coordination unit"],
+    "Public entity users": x["Public entities other than the Data Exchange operator use the system"],
+    "Impact metrics exist": x["Impact metrics of the Data Exchange exist"],
+    "Additional Info": x["0 Additional Information"],
+    "Notes": x["0 Notes"],
+  }
+})
+
+export const DataExchangeFlags = [
+  'Semantic interoperability',
+  'Real-time',
+  'Scalable architecture',
+  'Onboarding docs',
+  'Auditable',
+  'Procedural rules established',
+  'Public entity users',
+  'Impact metrics exist',
+]
+
+export const DataExchangeText = [
+  'Governing Entity',
+  'Ownership',
+  'Allowed users',
+  'National or Regional',
+  'Sector-specific or Cross-sectoral',
+  'Coordination unit'
+]
