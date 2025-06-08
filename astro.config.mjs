632b5c94880d94f7f29e6f7f7faf7f899869d9c7
Oli Evans <oli@protocol.ai>
2025-06-08 22:30:15 +0100

    chore: make url detection work in pages
    License: MIT
    Signed-off-by: Oli Evans <oli@zilla.org.uk>

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,17 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 import mdx from '@astrojs/mdx';
 
-import { execSync } from 'node:child_process'
-
-function executeGitCommand(command) {
-  return execSync(command)
-    .toString('utf8')
-    .replace(/[\n\r\s]+$/, '');
-}
-
-const branch = executeGitCommand('git rev-parse --abbrev-ref HEAD')
-
-const site = branch === 'main' ? 'https://dpi-map.pages.dev' : `https://${branch}.dpi-map.pages.dev`
+const site = process.env.CF_PAGES_URL ?? 'https://dpi-map.pages.dev'
 
 // https://astro.build/config
 export default defineConfig({
