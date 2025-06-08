02d1922cb37ca04db2bee605fbc89c9f00196875
Oli Evans <oli@protocol.ai>
2025-06-08 22:20:40 +0100

    feat: dynamic og image from svg
    - use `experimental_AstroContainer` to render the map svg to a string
    - use `sharp` to convert the svg to png, crop and optimise it.
    
    License: MIT
    Signed-off-by: Oli Evans <oli@zilla.org.uk>

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,25 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
-import react from '@astrojs/react';
-
 import mdx from '@astrojs/mdx';
+
+import { execSync } from 'node:child_process'
+
+function executeGitCommand(command) {
+  return execSync(command)
+    .toString('utf8')
+    .replace(/[\n\r\s]+$/, '');
+}
+
+const branch = executeGitCommand('git rev-parse --abbrev-ref HEAD')
+
+const site = branch === 'main' ? 'https://dpi-map.pages.dev' : `https://${branch}.dpi-map.pages.dev`
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://dpi-map.pages.dev',
+  site,
   vite: {
     plugins: [tailwindcss()]
   },
-
-  integrations: [react(), mdx()]
+  integrations: [mdx()]
 });
